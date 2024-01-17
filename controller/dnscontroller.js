bf7e1fe8fd63bc29261fd2f0925ed6021047d2dd
Pravin Nichal <nichal.pravin2001@gmail.com>
2024-01-17 12:14:02 +0530

    commit

--- controller/dnscontroller.js
+++ controller/dnscontroller.js
@@ -1,10 +1,18 @@
+import { saveRecordService } from "../services/dnsservice.js";
+
 export const getRecord = async (req, res) => {
   console.log(req.params.id);
   return res.send({ message: "Message" });
 };
 export const saveRecord = async (req, res) => {
   console.log(req.body);
-  return res.send({ message: "Message" });
+  await saveRecordService(req.body)
+    .then((response) => {
+      return res.status(response.status).json(response);
+    })
+    .catch((err) => {
+      return res.status(err.status).json(err);
+    });
 };
 export const updateRecord = async (req, res) => {
   console.log(req.body);
--- services/dnsservice.js
+++ services/dnsservice.js
@@ -1,1 +1,25 @@
-export const getDnsService = () => {};
+import { Record } from "../models/dns.js";
+
+export const saveRecordService = (record) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const recordStore = Record({ ...record });
+      await recordStore.save();
+      return resolve({
+        status: 201,
+        message: "Record saved successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      if (error.code === 11000)
+        return reject({
+          status: 402,
+          message: "Record already exists",
+        });
+      return reject({
+        status: 402,
+        message: "Failed to save record",
+      });
+    }
+  });
+};
